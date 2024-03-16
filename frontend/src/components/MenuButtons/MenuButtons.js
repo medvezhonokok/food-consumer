1d7873cf434b1955a69db050b8fb3465ec12fbcc
Michael Kim <nocap239@gmail.com>
2024-03-16 20:37:47 +0300

    + build ))

--- frontend/src/components/MenuButtons/MenuButtons.js
+++ frontend/src/components/MenuButtons/MenuButtons.js
@@ -8,7 +8,7 @@ const MenuButtons = ({handleLogout}) => {
             <Button href={'orders'} className="btn btn-primary" style={{width: '10rem'}}>Orders</Button>
             <Button href={'schedule'} className="btn btn-primary" style={{width: '10rem'}}>Schedule</Button>
             <Button href={'users'} className="btn btn-primary" style={{width: '10rem'}}>All users</Button>
-            <Button href={'http://http://5.101.51.223/:6060'} className="btn btn-primary" style={{width: '10rem'}}>Chat</Button>
+            <Button href={'http://5.101.51.223:6060'} className="btn btn-primary" style={{width: '10rem'}}>Chat</Button>
             <Button href={'profile'} className="btn btn-primary" style={{width: '10rem'}}>Profile</Button>
             <Button onClick={handleLogout} className="btn btn-primary" style={{width: '10rem'}}>Log out</Button>
         </Stack>
