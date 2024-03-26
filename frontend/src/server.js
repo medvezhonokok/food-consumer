8d9f5d3c7e412e615542971c2716731cc00ca562
Michael Kim <nocap239@gmail.com>
2024-03-27 02:35:41 +0300

    tiny fixes

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "http://5.101.51.223:8000",
         methods: ["GET", "POST"],
     },
 });
