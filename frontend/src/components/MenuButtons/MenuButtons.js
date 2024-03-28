0903ef70435369c68fe5671467a8b6ca17e79a5b
rzvrt <murderedpain@gmail.com>
2024-03-29 01:33:14 +0300

    updates

--- frontend/src/components/MenuButtons/MenuButtons.js
+++ frontend/src/components/MenuButtons/MenuButtons.js
@@ -1,29 +1,21 @@
 import React from 'react';
-import { Button, Stack } from "react-bootstrap";
-import { Link } from 'react-router-dom';
-import styles from './MenuButtons.module.css';
+import {Button, Stack} from "react-bootstrap";
 
 const MenuButtons = () => {
-    let style = {
-        width: '13rem',
-        height: '3.5rem',
-        fontSize: '1.5rem',
-        borderRadius: '0.8rem',
-        animationDelay: '0.2s'
-    };
-
-    return (
-        <div style={{ marginTop: '8rem', marginBottom: '3rem'}}>
-            <Stack direction="vertical" gap={3} className={`d-flex flex-column align-items-center`}>
-                <Link to="/tasks" className={`btn btn-primary btn-lg ${styles.animatedButton}`} style={style}>Tasks</Link>
-                <Link to="/orders" className={`btn btn-primary btn-lg ${styles.animatedButton}`} style={style}>Orders</Link>
-                <Link to="/schedule" className={`btn btn-primary btn-lg ${styles.animatedButton}`} style={style}>Schedule</Link>
-                <Link to="/users" className={`btn btn-primary btn-lg ${styles.animatedButton}`} style={style}>All users</Link>
-                <Link to="/chat" className={`btn btn-primary btn-lg ${styles.animatedButton}`} style={style}>Chat</Link>
-                <Link to="/profile" className={`btn btn-primary btn-lg ${styles.animatedButton}`} style={style}>Profile</Link>
-            </Stack>
-        </div>
-    );
+    return (<div style={{marginTop: '5rem'}}>
+        <Stack direction="vertical" gap={3} className="d-flex flex-column align-items-center">
+            <Button href={'tasks'} className="btn btn-primary" style={{width: '10rem'}}>Tasks</Button>
+            <Button href={'orders'} className="btn btn-primary" style={{width: '10rem'}}>Orders</Button>
+            <Button href={'schedule'} className="btn btn-primary" style={{width: '10rem'}}>Schedule</Button>
+            <Button href={'users'} className="btn btn-primary" style={{width: '10rem'}}>All users</Button>
+            <Button href={'chat'} className="btn btn-primary" style={{width: '10rem'}}>Chat</Button>
+            <Button href={'profile'} className="btn btn-primary" style={{width: '10rem'}}>Profile</Button>
+        </Stack>
+    </div>)
 };
+
+MenuButtons.propTypes = {};
+
+MenuButtons.defaultProps = {};
 
 export default MenuButtons;
