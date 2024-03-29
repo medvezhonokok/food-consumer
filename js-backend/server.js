d372a4a66c385e51cc2bae0cc886de97ea32de66
Michael Kim <nocap239@gmail.com>
2024-03-29 03:11:17 +0300

    ads

--- js-backend/server.js
+++ js-backend/server.js
@@ -19,7 +19,7 @@ const io = new Server(server, {
 });
 
 const connection = mysql.createConnection({
-    host: "maria-db",
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
