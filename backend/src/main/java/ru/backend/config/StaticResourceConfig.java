89b7ae55840b0f2d29f00a2b6d89d75527e3aa20
rzvrt <murderedpain@gmail.com>
2024-04-03 17:52:13 +0300

    fixes

--- backend/src/main/java/ru/backend/config/StaticResourceConfig.java
+++ backend/src/main/java/ru/backend/config/StaticResourceConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class StaticResourceConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/news/**")
-                .addResourceLocations("classpath:/static/news/");
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/");
     }
 }
