d4c649476d83f9a6fe05eef78d84e282028c8966
Michael Kim <nocap239@gmail.com>
2024-04-03 18:42:32 +0300

    fixes

--- backend/src/main/java/ru/backend/service/FileService.java
+++ backend/src/main/java/ru/backend/service/FileService.java
@@ -6,47 +6,25 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 @Service
 public class FileService {
 
     public String saveImageToStorage(MultipartFile imageFile) throws IOException {
-        String uploadDirectory = "src/main/resources/static/images";
+        String uploadDirectory = "/root/food-consumer/backend/src/main/resources/static/news";
         String uniqueFileName = UUID.randomUUID() + "_" + imageFile.getOriginalFilename();
 
-        Path uploadPath = Path.of(uploadDirectory);
+        Path uploadPath = Paths.get(uploadDirectory);
         Path filePath = uploadPath.resolve(uniqueFileName);
 
         if (!Files.exists(uploadPath)) {
             Files.createDirectories(uploadPath);
         }
 
-        Files.copy(imageFile.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(imageFile.getInputStream(), filePath);
 
         return uniqueFileName;
-    }
-
-    public byte[] getImage(String imageDirectory, String imageName) throws IOException {
-        Path imagePath = Path.of(imageDirectory, imageName);
-
-        if (Files.exists(imagePath)) {
-            byte[] imageBytes = Files.readAllBytes(imagePath);
-            return imageBytes;
-        } else {
-            return null; // Handle missing images
-        }
-    }
-
-    public String deleteImage(String imageDirectory, String imageName) throws IOException {
-        Path imagePath = Path.of(imageDirectory, imageName);
-
-        if (Files.exists(imagePath)) {
-            Files.delete(imagePath);
-            return "Success";
-        } else {
-            return "Failed"; // Handle missing images
-        }
     }
 }
