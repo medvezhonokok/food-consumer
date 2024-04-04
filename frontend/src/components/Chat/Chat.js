5a249b7a9a7ab8c4a997bdb97d99678cb93d897a
Michael Kim <nocap239@gmail.com>
2024-04-04 13:21:01 +0300

    updates %^)

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './Chat.module.css';
 import io from "socket.io-client";
 import Button from '@mui/material/Button';
@@ -7,7 +7,7 @@ import CustomNavbar from "../CustomNavbar/CustomNavbar";
 
 const socket = io.connect(process.env.REACT_APP_SOCKET_URL);
 
-const Chat = ({user}) => {
+const Chat = ({ user }) => {
     const [message, setMessage] = useState("");
     const [messageHistory, setMessageHistory] = useState([]);
     const messagesEndRef = useRef(null);
@@ -18,7 +18,7 @@ const Chat = ({user}) => {
         if (!message.trim()) {
             return;
         }
-        socket.emit("send_message", {text: message.trim(), sender: user});
+        socket.emit("send_message", { text: message.trim(), sender: user });
         setMessage("");
         textareaRef.current.style.height = '2.5rem';
         setRows(1);
@@ -71,7 +71,7 @@ const Chat = ({user}) => {
     }, [messageHistory]);
 
     const scrollToBottom = () => {
-        messagesEndRef.current?.scrollIntoView({behavior: "smooth"});
+        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
     };
 
     function getTimeByDate(createdAt) {
@@ -80,6 +80,72 @@ const Chat = ({user}) => {
         const minutes = date.getMinutes().toString().padStart(2, '0');
         return `${hours}:${minutes}`;
     }
+
+    const isSameDay = (date1, date2) => {
+        return (
+            date1.getFullYear() === date2.getFullYear() &&
+            date1.getMonth() === date2.getMonth() &&
+            date1.getDate() === date2.getDate()
+        );
+    };
+
+    const renderMessagesWithDates = () => {
+        let lastMessageDate = null;
+
+        const formatDate = (date) => {
+            const options = { day: 'numeric', month: 'long' };
+            return new Intl.DateTimeFormat('ru-RU', options).format(date);
+        };
+
+        return messageHistory.map((message, index) => {
+            const messageDate = new Date(message.createdAt);
+            const showDate = !lastMessageDate || !isSameDay(lastMessageDate, messageDate);
+
+            lastMessageDate = messageDate;
+
+            return (
+                <React.Fragment key={index}>
+                    {showDate && (
+                        <div className={styles.Chat__date}>
+                            {formatDate(messageDate)}
+                        </div>
+                    )}
+                    <div className={styles.Chat__message}>
+                        {(message.sender.name && message.sender.name === user.name) || message.sender === user.name ? (
+                            <div className={`${styles.Chat__message} ${styles.Chat__myMessage}`}>
+                                <div style={{ marginBottom: "0.3rem" }}>
+                                    {message.text}
+                                </div>
+                                <div className={`${styles.time}`}>
+                                    {getTimeByDate(message.createdAt)}
+                                </div>
+                            </div>
+                        ) : (
+                            <div className={`${styles.Chat__message} ${styles.Chat__otherMessage}`}>
+                                <div className={styles.messageBox}>
+                                    <div className={styles.flexContainer}>
+                                        <div className={styles.avatar}>
+                                            <img src={"/avatars/" + message.sender_login + ".JPG"} alt="Avatar"/>
+                                        </div>
+                                        <div className={styles.name}>
+                                            {message.sender.name ? message.sender.name : message.sender}
+                                        </div>
+                                    </div>
+                                    <div className={styles.messageText}>
+                                        {message.text}
+                                    </div>
+                                    <div className={styles.time}>
+                                        {getTimeByDate(message.createdAt)}
+                                    </div>
+                                </div>
+
+                            </div>
+                        )}
+                    </div>
+                </React.Fragment>
+            );
+        });
+    };
 
     const handleTextareaInput = (event) => {
         const textarea = event.target;
@@ -93,54 +159,22 @@ const Chat = ({user}) => {
             ? <>
                 <CustomNavbar user={user}/>
                 <div className={styles.Chat}>
-                    <div className={styles.Chat__messages} style={{animation: 'scroll 0.5s linear'}}>
-                        {messageHistory.map((message, index) => (
-                            <div key={index} className={styles.Chat__message}>
-                                {((message.sender.name && message.sender.name === user.name) || message.sender === user.name)
-                                    ? (<>
-                                        <div className={`${styles.Chat__message} ${styles.Chat__myMessage}`}>
-                                            <div style={{marginBottom: "0.3rem"}}>
-                                                {message.text}
-                                            </div>
-                                            <div className={`${styles.time}`}>
-                                                {getTimeByDate(message.createdAt)}
-                                            </div>
-                                        </div>
-                                    </>)
-                                    : (
-                                        <div className={`${styles.Chat__otherMessage}`}>
-                                            <div className={styles.avatar}>
-                                                <img src={"/avatars/" + message.sender_login + ".JPG"} alt="Avatar"/>
-                                            </div>
-                                            <div className={styles.messageBox}>
-                                                <div className={styles.author}>
-                                                    {(message.sender.name ? message.sender.name : message.sender)}
-                                                </div>
-                                                <div>
-                                                    {message.text}
-                                                </div>
-                                                <div className={styles.time}>
-                                                    {getTimeByDate(message.createdAt)}
-                                                </div>
-                                            </div>
-                                        </div>
-                                    )}
-                            </div>
-                        ))}
-                        <div ref={messagesEndRef}/>
+                    <div className={styles.Chat__messages} style={{ animation: 'scroll 0.5s linear' }}>
+                        {renderMessagesWithDates()}
+                        <div ref={messagesEndRef} />
                     </div>
                     <div className={styles.Chat__input}>
-                    <textarea
-                        ref={textareaRef}
-                        placeholder="Message..."
-                        value={message}
-                        onChange={(event) => setMessage(event.target.value)}
-                        onInput={handleTextareaInput}
-                        rows={rows}
-                        style={{margin: "0.3rem 0.4rem", resize: "none", overflow: "hidden"}}
-                    />
-                        <Button variant="contained" endIcon={<SendIcon/>} onClick={sendMessage}
-                                style={{height: "2.5rem", background: "#524e4e", margin: "auto 0.3rem auto 0"}}/>
+                        <textarea
+                            ref={textareaRef}
+                            placeholder="Message..."
+                            value={message}
+                            onChange={(event) => setMessage(event.target.value)}
+                            onInput={handleTextareaInput}
+                            rows={rows}
+                            style={{ margin: "0.3rem 0.4rem", resize: "none", overflow: "hidden" }}
+                        />
+                        <Button variant="contained" endIcon={<SendIcon />} onClick={sendMessage}
+                                style={{ height: "2.5rem", background: "#524e4e", margin: "auto 0.3rem auto 0" }} />
                     </div>
                 </div>
             </>
