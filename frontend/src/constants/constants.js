251e67b923f979928a3187c497e0d4607be08c3d
Michael Kim <nocap239@gmail.com>
2024-04-19 23:18:12 +0300

    1231

--- frontend/src/constants/constants.js
+++
@@ -1,2 +0,0 @@
-export const BACKEND_JAVA_URL = 'http://5.101.51.223:8080/api'
-export const BACKEND_JS_URL = 'http://5.101.51.223:3001'
--- frontend/src/data/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import orders from "./../reducers/orders";
-
-export default configureStore({
-    reducer: {
-        orders: orders,
-    },
-})
