27d861d629547a41a5613e75cf7f0fb6f306fdf7
Michael Kim <90005242+medvezhonokok@users.noreply.github.com>
2024-04-19 22:02:46 +0300

    Delete frontend directory

--- frontend/README.md
+++
@@ -1,2 +0,0 @@
-Запуск `npm run start`
-Для создания компонентов используем `npx generate-react-cli component Task` 
--- frontend/src/store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import ordersReducer from "./reducers/orders";
-
-export default configureStore({
-    reducer: {
-        orders: ordersReducer,
-    },
-})
